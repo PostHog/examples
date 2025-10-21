127bb55a8339b0eb0ae7d5e54641c0b5c586dc4f
Vincent (Wen Yu) Ge <gewenyu99@gmail.com>
2025-10-20 21:39:23 -0400

    fix job

--- basics/next-app-router/instrumentation-client.ts
+++ basics/next-app-router/instrumentation-client.ts
@@ -1,13 +1,5 @@
 import posthog from "posthog-js"
 
-// Debug environment variables
-console.log('🔍 Environment Debug Info:');
-console.log('  - NODE_ENV:', process.env.NODE_ENV);
-console.log('  - POSTHOG_TEST_MODE:', process.env.POSTHOG_TEST_MODE);
-console.log('  - Is Test Mode:', process.env.POSTHOG_TEST_MODE === 'true');
-
-const isTestMode = process.env.POSTHOG_TEST_MODE === 'true';
-
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: "/ingest",
   ui_host: "https://us.posthog.com",
@@ -18,6 +10,6 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   // Turn on debug in development mode
   debug: process.env.NODE_ENV === "development",
   // Disable request batching in test environment
-  request_batching: true,
-  opt_out_useragent_filter: true,  // This disables bot detection
+  request_batching: process.env.CI ? false : true,
+  opt_out_useragent_filter: process.env.CI ? true : false,  // This disables bot detection
 });
